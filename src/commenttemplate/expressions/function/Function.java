4011e400154bf09e140a3501e1b90683d156b083
thiago <thiagorabelo@ifpi.edu.br>
2015-05-14 17:20:12 -0300

    A classe commenttemplate.expression.function.Function foi documentada.

--- src/commenttemplate/expressions/function/Function.java
+++ src/commenttemplate/expressions/function/Function.java
@@ -6,27 +6,55 @@ import java.util.List;
 import commenttemplate.expressions.tree.Exp;
 
 /**
+ * A function is a class that extends this Function class and implements the
+ * "eval" method of Exp inteface and has a list of parameters of Exp type.
+ * 
  * @author thiago
  */
 public abstract class Function implements Exp {
 
+	/**
+	 * The list of parameters of the function.
+	 */
 	private List<Exp> args = new ArrayList<Exp>();
 
+	/**
+	 * The default constructor.
+	 */
 	public Function() {
 	}
 
+	/**
+	 * Return the list of arguments.
+	 * @return The list of arguments.
+	 */
 	public List<Exp> getArgs() {
 		return args;
 	}
 	
+	/**
+	 * Set the list of arguments.
+	 * 
+	 * @param args The new list of arguments.
+	 */
 	public void setArgs(List<Exp> args) {
 		this.args = args;
 	}
 
+	/**
+	 * Convert a array of Exp in a list of function arguments.
+	 * 
+	 * @param args The array to be converted to a new list of arguments.
+	 */
 	public void setArgs(Exp [] args) {
 		this.args = new ArrayList<Exp>(Arrays.asList(args));
 	}
 	
+	/**
+	 * Append some Exp object in list arguments.
+	 * 
+	 * @param arg The Exp object to be appended to the list arguments.
+	 */
 	public void appendArg(Exp arg) {
 		if (args == null) {
 			args = new ArrayList<Exp>();
@@ -34,6 +62,9 @@ public abstract class Function implements Exp {
 		args.add(arg);
 	}
 	
+	/**
+	 * Trim the list of arguments to save memory.
+	 */
 	public void trim() {
 		try {
 			((ArrayList<Exp>)this.args).trimToSize();
@@ -42,6 +73,12 @@ public abstract class Function implements Exp {
 		}
 	}
 
+	/**
+	 * Convert this Function instance in String, putting the result in the
+	 * StringBuilder passed as parameter.
+	 * 
+	 * @param sb A StringBuilder which the String result must be put.
+	 */
 	@Override
 	public void toString(StringBuilder sb) {
 		sb.append("(");
@@ -56,6 +93,11 @@ public abstract class Function implements Exp {
 		sb.append(")");
 	}
 
+	/**
+	 * Converte esta função em um String.
+	 * 
+	 * @return A String that represents this function.
+	 */
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
